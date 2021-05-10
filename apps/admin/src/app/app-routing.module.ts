e042d4e1e10548df1751ce35cc2857fed3b259fd
Zack <zackarychapple30@gmail.com>
2021-05-10 10:39:07 -0400

    feat(core): adding base styles from library

--- apps/admin/src/app/app-routing.module.ts
+++ apps/admin/src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
 
 
+const routes: Routes = [
+  {
+    path: '404', loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule)
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
+    RouterModule.forRoot(routes),
     CommonModule
   ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, RouterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
