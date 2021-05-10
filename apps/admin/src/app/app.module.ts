cf13189b0858be83bcbe6eb0048f0a4910cbc3d2
Zack <zackarychapple30@gmail.com>
2021-05-10 10:55:09 -0400

    feat(core): added footer

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import {RouterModule} from "@angular/router";
+import {FooterModule} from "./core/footer/footer.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, RouterModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, RouterModule, FooterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
