859c551a8bad98d4d111b62903e17fcc1d1e9c90
Zack <zackarychapple30@gmail.com>
2021-05-13 13:33:14 -0400

    feat(projects): adding randomness to the number of comments

--- apps/api/src/projects/projects.seeds.ts
+++ apps/api/src/projects/projects.seeds.ts
@@ -4,9 +4,11 @@ import {CommentEntity} from "./comment.entity";
 
 export class CreateProjects implements Seeder {
   public async run(factory: Factory): Promise<void> {
-    const projectZ = await factory(ProjectsEntity)()
+    await factory(ProjectsEntity)()
       .map(async project => {
-        project.comments = await factory(CommentEntity)({"parent_id": project.id}).createMany(2);
+        project.comments = await factory(CommentEntity)({"parent_id": project.id})
+          .createMany(Math.floor(Math.random() * 10));
+
         return project;
       })
       .createMany(20)
