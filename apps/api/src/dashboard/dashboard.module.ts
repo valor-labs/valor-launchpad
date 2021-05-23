93d81263acd8c3e1573c710d204963e211e4fc75
steve <xhysteve@gmail.com>
2021-05-23 23:43:00 +0800

    feat(projects): move data to NestJS service

--- apps/api/src/dashboard/dashboard.module.ts
+++ apps/api/src/dashboard/dashboard.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DashboardController } from './dashboard.controller';
+import { DashboardService } from './dashboard.service';
 
 @Module({
-  controllers: [DashboardController]
+  controllers: [DashboardController],
+  providers: [DashboardService]
 })
 export class DashboardModule {}
