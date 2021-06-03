0651cebcc6489c5c5606abf99ef58123b8073cb9
Zack <zackarychapple30@gmail.com>
2021-06-03 19:07:46 -0400

    feat(stripe): stripe payments working for test

--- libs/stripe-api/src/lib/stripe.controller.ts
+++ libs/stripe-api/src/lib/stripe.controller.ts
@@ -5,10 +5,24 @@ import {RequestWithSession} from '../../../../apps/api/src/common/RequestWithSes
 @Controller('v1')
 export class StripeController {
   stripe: Stripe;
-  endpointSecret='whsec_CfYnVo8i3Q6h40NAxfmMvwIGTsEdBDmb';
+  endpointSecret = 'whsec_CfYnVo8i3Q6h40NAxfmMvwIGTsEdBDmb';
+
   constructor() {
-    this.stripe = new Stripe('pk_test_51IyGuEAcm152H20WJusvJbWOGaqsdj4TXzS0cQtSEHD3jE9GGQJ0hay5Tn8i5h3IL8TShk4XKd5VghIKlHxo2gvT00IDgRx1Bu',
+    this.stripe = new Stripe('sk_test_51IyGuEAcm152H20W2X1CYZjtI2PAamDitY4gBlLVkoT8LeYc1WA3xT6dBCz1rOLmFEMfrqzo0AopbMwO6wVdxIvn00T63xshwx',
       {apiVersion: '2020-08-27'})
+  }
+
+  @Post('payment_intents')
+  async createPaymentIntents(@Body() body, @Res() res) {
+    const {items} = body;
+    // Create a PaymentIntent with the order amount and currency
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      amount: this.calculateOrderAmount(items),
+      currency: "usd"
+    });
+    res.send({
+      clientSecret: paymentIntent.client_secret
+    });
   }
 
   @Post('create-checkout-session')
@@ -33,4 +47,11 @@ export class StripeController {
       cancel_url: `localhost:4200/cancel.html`,
     })
   }
+
+  calculateOrderAmount(items) {
+    // Replace this constant with a calculation of the order's amount
+    // Calculate the order total on the server to prevent
+    // people from directly manipulating the amount on the client
+    return 1400;
+  };
 }
