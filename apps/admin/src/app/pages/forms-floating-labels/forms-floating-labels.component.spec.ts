fca64463a663b747e040cf0463e05096a327a710
ErikMac <erikyuyz@gmail.com>
2021-06-06 10:34:23 +0800

    feat(floating-labels): init floating labels page

---
+++ apps/admin/src/app/pages/forms-floating-labels/forms-floating-labels.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FormsFloatingLabelsComponent } from './forms-floating-labels.component';
+
+describe('FormsFloatingLabelsComponent', () => {
+  let component: FormsFloatingLabelsComponent;
+  let fixture: ComponentFixture<FormsFloatingLabelsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FormsFloatingLabelsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FormsFloatingLabelsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- apps/admin/src/app/pages/forms-floating-labels/forms-floating-labels.module.ts
+++ apps/admin/src/app/pages/forms-floating-labels/forms-floating-labels.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsFloatingLabelsComponent } from './forms-floating-labels.component';
+import { FormsFloatingLabelsRoutingModule } from './forms-floating-labels-routing.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FormsFloatingLabelsComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsFloatingLabelsRoutingModule,
   ]
 })
 export class FormsFloatingLabelsModule { }
