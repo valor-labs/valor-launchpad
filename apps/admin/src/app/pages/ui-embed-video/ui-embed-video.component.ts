0ae0b86f07fd7fd141d00c0f236af4c5595978c6
steve <xhysteve@gmail.com>
2021-07-09 16:49:59 +0800

    Fixes #106

---
+++ apps/admin/src/app/pages/ui-embed-video/ui-embed-video.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'valor-launchpad-ui-embed-video',
+  templateUrl: './ui-embed-video.component.html',
+  styleUrls: ['./ui-embed-video.component.scss']
+})
+export class UiEmbedVideoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- apps/admin/src/app/pages/ui-embed-video/ui-embed-video.module.ts
+++ apps/admin/src/app/pages/ui-embed-video/ui-embed-video.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UiEmbedVideoRoutingModule } from './ui-embed-video-routing.module';
+import { UiEmbedVideoComponent } from './ui-embed-video.component';
+import { UiModule } from '@valor-launchpad/ui';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UiEmbedVideoComponent
+  ],
   imports: [
     CommonModule,
-    UiEmbedVideoRoutingModule
+    UiEmbedVideoRoutingModule,
+    UiModule
   ]
 })
 export class UiEmbedVideoModule { }
