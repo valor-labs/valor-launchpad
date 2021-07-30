10f08b5d4455cf02104f163836d1205cd0a861c4
Zack <zackarychapple30@gmail.com>
2021-07-29 20:48:27 -0400

    chore(cleanup): adding text to footer

--- apps/admin/src/app/core/footer/footer.component.ts
+++ apps/admin/src/app/core/footer/footer.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 
 @Component({
   selector: 'valor-launchpad-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FooterComponent implements OnInit {
-
+export class FooterComponent {
+  currentYear = new Date().getFullYear()
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
