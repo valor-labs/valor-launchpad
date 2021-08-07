f6e454a9ec40f3e2ed242d60c3b38ff5faac1b1c
ErikMac <erikyuyz@gmail.com>
2021-08-07 14:43:19 +0800

    feat(reset-password): Convert Reset Password Page into Angular way

--- apps/admin/src/app/pages/reset-password/reset-password.component.ts
+++ apps/admin/src/app/pages/reset-password/reset-password.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ResetPasswordService} from './reset-password.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'valor-launchpad-reset-password',
@@ -7,16 +7,23 @@ import {ResetPasswordService} from './reset-password.service';
   styleUrls: ['./reset-password.component.scss']
 })
 export class ResetPasswordComponent implements OnInit {
+  formGroup: FormGroup;
   message = 'Please enter your username to reset your password.'
-  constructor(private resetPasswordService: ResetPasswordService) { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formGroup = this.fb.group({
+      email: [null, [Validators.required, Validators.email]],
+    })
   }
 
-  resetPassword(resetPasswordForm){
-    this.message = 'Requesting your password reset';
-    this.resetPasswordService.resetPassword(resetPasswordForm.value.username).subscribe(()=>{
-      this.message = 'Your password has been reset please check your email!'
-    })
+  resetPassword() {
+    for (const ctrl of Object.values(this.formGroup.controls)) {
+      ctrl.markAsDirty();
+    }
+    if (this.formGroup.invalid) {
+      return;
+    }
+    console.log(this.formGroup.value);
   }
 }
