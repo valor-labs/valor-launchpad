2d28ecfe496dec2101d1b7b41727dbced8fd27e9
ErikMac <erikyuyz@gmail.com>
2021-08-09 15:10:17 +0800

    feat(dashboard-social): Move data to angular service

--- apps/admin/src/app/pages/dashboard-social/dashboard-social.component.ts
+++ apps/admin/src/app/pages/dashboard-social/dashboard-social.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ISocialActivity, ISocialUser, ISocialUserInfo, IStory } from './dashboard-social.model';
+import { DashboardSocialService } from './dashboard-social.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'valor-launchpad-dashboard-social',
@@ -8,125 +10,22 @@ import { ISocialActivity, ISocialUser, ISocialUserInfo, IStory } from './dashboa
 })
 export class DashboardSocialComponent implements OnInit {
 
-  stories: IStory[] = [
-    {
-      avatarUrl: 'assets/img/avatars/avatar-5.jpg',
-      username: 'Ashley Briggs',
-      createdAt: new Date(),
-      content: 'Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus.',
-      images: [
-        'assets/img/photos/unsplash-2.jpg',
-        'assets/img/photos/unsplash-3.jpg',
-      ],
-      comments: [
-        {
-          username: 'Stacie Hall',
-          content: 'Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam ultrices mauris. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris.',
-          avatarUrl: 'assets/img/avatars/avatar-4.jpg',
-        },
-      ],
-    },
-    {
-      avatarUrl: 'assets/img/avatars/avatar.jpg',
-      username: 'Chris Wood',
-      createdAt: new Date(),
-      content: 'Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante.',
-      images: [],
-      comments: [],
-    },
-    {
-      avatarUrl: 'assets/img/avatars/avatar-2.jpg',
-      username: 'Carl Jenkins',
-      createdAt: new Date(),
-      content: '',
-      images: ['assets/img/photos/unsplash-1.jpg'],
-      comments: [
-        {
-          username: 'Stacie Hall',
-          avatarUrl: 'assets/img/avatars/avatar-4.jpg',
-          content: 'Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam ultrices mauris. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris.',
-        }
-      ],
-    },
-    {
-      avatarUrl: 'assets/img/avatars/avatar-5.jpg',
-      username: 'Ashley Briggs',
-      createdAt: new Date(),
-      content: ' Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante.',
-      images: [],
-      comments: [],
-    }
-  ];
+  stories$: Observable<IStory[]>;
 
-  userinfo: ISocialUserInfo = {
-    username: 'Chris Wood',
-    avatarUrl: 'assets/img/avatars/avatar.jpg',
-    job: 'UX Engineer',
-    followed: false,
-  };
+  userinfo$: Observable<ISocialUserInfo>;
 
-  followings: ISocialUser[] = [
-    {username: 'Ashley Briggs', avatarUrl: 'assets/img/avatars/avatar-5.jpg', followed: true},
-    {username: 'Carl Jenkins', avatarUrl: 'assets/img/avatars/avatar-2.jpg', followed: true},
-    {username: 'Stacie Hall', avatarUrl: 'assets/img/avatars/avatar-4.jpg', followed: true},
-  ];
+  followings$: Observable<ISocialUser[]>;
 
-  activities: ISocialActivity[] = [
-    {
-      username: 'Ashley Briggs',
-      avatarUrl: 'assets/img/avatars/avatar-5.jpg',
-      action: 'startFollowing',
-      createdAt: new Date(),
-      targetUsername: 'Stacie Hall',
-    },
-    {
-      username: 'Chris Wood',
-      avatarUrl: 'assets/img/avatars/avatar.jpg',
-      action: 'postOnTimeline',
-      createdAt: new Date(),
-      targetUsername: 'Stacie Hall',
-      postDetails: {
-        content: 'Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam..'
-      }
-    },
-    {
-      username: 'Stacie Hall',
-      avatarUrl: 'assets/img/avatars/avatar-4.jpg',
-      action: 'postBlog',
-      createdAt: new Date(),
-    },
-    {
-      username: 'Carl Jenkins',
-      avatarUrl: 'assets/img/avatars/avatar-2.jpg',
-      action: 'postOnTimeline',
-      createdAt: new Date(),
-      targetUsername: 'Stacie Hall',
-      postDetails: {
-        photos: [
-          'assets/img/photos/unsplash-1.jpg',
-          'assets/img/photos/unsplash-2.jpg',
-        ]
-      }
-    },
-    {
-      username: 'Stacie Hall',
-      avatarUrl: 'assets/img/avatars/avatar-4.jpg',
-      action: 'postBlog',
-      createdAt: new Date(),
-    },
-    {
-      username: 'Chris Wood',
-      avatarUrl: 'assets/img/avatars/avatar.jpg',
-      action: 'startFollowing',
-      createdAt: new Date(),
-      targetUsername: 'Stacie Hall',
-    },
-  ];
+  activities$: Observable<ISocialActivity[]>;
 
 
-  constructor() { }
+  constructor(private dashboardSocialService: DashboardSocialService) { }
 
   ngOnInit(): void {
+    this.stories$ = this.dashboardSocialService.fetchTimeline();
+    this.userinfo$ = this.dashboardSocialService.fetchUserInfo();
+    this.followings$ = this.dashboardSocialService.fetchFollowings();
+    this.activities$ = this.dashboardSocialService.fetchActivities();
   }
 
 }
