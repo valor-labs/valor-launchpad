5f0a16542440e5bbfad34c5313fd487fa3f60d62
liu yi <yolaucn@gmail>
2021-08-09 21:58:05 +0800

    fix dropdown overlop bug #187

--- libs/ui/src/lib/dropdown/dropdown.component.ts
+++ libs/ui/src/lib/dropdown/dropdown.component.ts
@@ -1,5 +1,9 @@
 import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 
+
+const $DropdownList=[];
+let $DropdownListIndex=0;
+
 @Component({
   selector: 'valor-launchpad-dropdown',
   templateUrl: './dropdown.component.html',
@@ -16,18 +20,30 @@ export class DropdownComponent implements OnInit, OnDestroy {
   size:''|'lg'=''
 
   show = false;
+  index:number=-1;
 
   constructor() {
+    this.index=$DropdownListIndex;
+    $DropdownList.push(this);
+    $DropdownListIndex++;
     this.handleClick = this.handleClick.bind(this);
   }
 
   ngOnDestroy() {
     document.removeEventListener('click', this.handleClick);
+    $DropdownList.splice(this.index,1);
   }
 
   toggleDropdown(e) {
     e.stopPropagation();
     this.show = !this.show;
+    if(this.show){
+      $DropdownList.forEach((instance)=>{
+        if(instance.index!==this.index){
+          instance.show=false
+        }
+      })
+    }
   }
 
   handleClick() {
