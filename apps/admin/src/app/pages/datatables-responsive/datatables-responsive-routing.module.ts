15285b7eee5b348d69abf6e2b2a46f7345dba0b4
xiehongyang <>
2021-08-18 17:17:11 +0800

    feat(page: responsive-table): done the page

--- apps/admin/src/app/pages/datatables-responsive/datatables-responsive-routing.module.ts
+++ apps/admin/src/app/pages/datatables-responsive/datatables-responsive-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TableResponsiveComponent } from './table-responsive/table-responsive.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TableResponsiveComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
---
+++ apps/admin/src/app/pages/datatables-responsive/table-responsive/table-responsive.component.ts
@@ -0,0 +1,50 @@
+import { Component, Inject, LOCALE_ID, OnInit } from '@angular/core';
+import { TableColumn } from '@swimlane/ngx-datatable/lib/types/table-column.type';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { tableData } from './fakeData';
+
+class CustomDatePipe extends DatePipe {
+  public transform(value): any {
+    return super.transform(value, 'y/MM/dd');
+  }
+}
+
+
+@Component({
+  selector: 'valor-launchpad-table-responsive',
+  templateUrl: './table-responsive.component.html',
+  styleUrls: ['./table-responsive.component.scss']
+})
+export class TableResponsiveComponent implements OnInit {
+
+  tableResponsiveData = tableData;
+  pageNumLimit = 10;
+  tableResponsiveColumns: Array<TableColumn> = [
+    { name: 'Name', prop: 'name' },
+    { name: 'Position', prop: 'position' },
+    { name: 'Office', prop: 'office' },
+    { name: 'Age', prop: 'age' },
+    {
+      name: 'Start date', prop: 'startDate', pipe: new CustomDatePipe(this.locale)
+    },
+    { name: 'Salary', prop: 'salary', pipe: new CurrencyPipe(this.locale) }
+  ];
+
+  constructor(
+    @Inject(LOCALE_ID) private locale: string
+  ) {
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  onChangeSearch(inputVal: string): void {
+    if (inputVal === '') {
+      this.tableResponsiveData = tableData;
+    } else {
+      this.tableResponsiveData = tableData.filter(rowData => Object.values(rowData).includes(inputVal));
+    }
+  }
+
+}
