3140b483df1c5317605aa5fc33b2eb266f695858
xiehongyang <>
2021-08-22 18:29:17 +0800

    feat(page: fixed-header-table): finish the page

--- apps/admin/src/app/pages/datatables-fixed-header/datatables-fixed-header-routing.module.ts
+++ apps/admin/src/app/pages/datatables-fixed-header/datatables-fixed-header-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DatatablesFixedHeaderComponent } from './datatables-fixed-header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DatatablesFixedHeaderComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- apps/admin/src/app/pages/datatables-fixed-header/datatables-fixed-header.module.ts
+++ apps/admin/src/app/pages/datatables-fixed-header/datatables-fixed-header.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DatatablesFixedHeaderRoutingModule } from './datatables-fixed-header-routing.module';
+import { DatatablesFixedHeaderComponent } from './datatables-fixed-header.component';
+import { FormsModule } from '@angular/forms';
+import { UiModule } from '@valor-launchpad/ui';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DatatablesFixedHeaderComponent
+  ],
   imports: [
     CommonModule,
-    DatatablesFixedHeaderRoutingModule
+    UiModule,
+    NgxDatatableModule,
+    DatatablesFixedHeaderRoutingModule,
+    FormsModule
   ]
 })
 export class DatatablesFixedHeaderModule { }
