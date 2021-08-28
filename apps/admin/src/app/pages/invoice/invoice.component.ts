c146a299a8e56ebe3761f66f3ddcdcd859dad23c
ErikMac <erikyuyz@gmail.com>
2021-08-28 13:52:41 +0800

    feat(invoice): move to angular binding and move data to angular service

--- apps/admin/src/app/pages/invoice/invoice.component.ts
+++ apps/admin/src/app/pages/invoice/invoice.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  Inject,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { InvoiceService } from './invoice.service';
 
 @Component({
   selector: 'valor-launchpad-invoice',
   templateUrl: './invoice.component.html',
-  styleUrls: ['./invoice.component.scss']
+  styleUrls: ['./invoice.component.scss'],
 })
 export class InvoiceComponent implements OnInit {
+  invoiceDetail$ = this.invoiceService.getInvoice();
 
-  constructor() { }
+  constructor(
+    @Inject(DOCUMENT) private _doc: Document,
+    private renderer: Renderer2,
+    private invoiceService: InvoiceService,
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onPrint() {
+    window.print();
   }
 
+  @HostListener('window:beforeprint')
+  onBeforePrint() {
+    const els = this.queryCommonEl();
+    els.forEach((el) => this.renderer.addClass(el, 'd-print-none'));
+  }
+
+  @HostListener('window:afterprint')
+  onAfterPrint() {
+    const els = this.queryCommonEl();
+    els.forEach((el) => this.renderer.removeClass(el, 'd-print-none'));
+  }
+
+  private queryCommonEl() {
+    return [
+      this._doc.querySelector('valor-launchpad-navigation'),
+      this._doc.querySelector('valor-launchpad-header'),
+      this._doc.querySelector('valor-launchpad-footer'),
+    ];
+  }
 }
