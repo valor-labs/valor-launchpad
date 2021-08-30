6c6f2e3df7490b5c87bea960a6db34baf9ca4738
liu yi <yolaucn@gmail>
2021-08-31 01:39:28 +0800

    datatable pages

--- apps/admin/src/app/pages/datatables-ajax/datatables-ajax-routing.module.ts
+++ apps/admin/src/app/pages/datatables-ajax/datatables-ajax-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DatatablesAjaxComponent } from './datatables-ajax.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'',
+  component:DatatablesAjaxComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- apps/admin/src/app/pages/datatables-column-search/datatables-column-search.module.ts
+++ apps/admin/src/app/pages/datatables-column-search/datatables-column-search.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DatatablesColumnSearchRoutingModule } from './datatables-column-search-routing.module';
+import { DatatablesColumnSearchComponent } from './datatables-column-search.component';
 
-
+import { FormsModule } from '@angular/forms';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { UiModule } from '@valor-launchpad/ui';
 @NgModule({
-  declarations: [],
+  declarations: [
+    DatatablesColumnSearchComponent
+  ],
   imports: [
     CommonModule,
-    DatatablesColumnSearchRoutingModule
+    DatatablesColumnSearchRoutingModule,
+    FormsModule,
+    NgxDatatableModule,
+    UiModule
   ]
 })
 export class DatatablesColumnSearchModule { }
