1d9e3856746ef6150a509a2e4e442886a8376787
xiehongyang <>
2021-09-18 11:34:13 +0800

    fix(forms-wizard): Forms-wizard page's wizard card are sharing same card instance. fixes #367

--- apps/admin/src/app/pages/forms-wizard/forms-wizard.component.ts
+++ apps/admin/src/app/pages/forms-wizard/forms-wizard.component.ts
@@ -7,8 +7,48 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./forms-wizard.component.scss']
 })
 export class FormsWizardComponent {
-  index = 1;
-  steps = [
+  primaryWizardIndex = 1;
+  primaryWizardSteps = [
+    {mainTitle: 'First step', description: 'Step description'},
+    {mainTitle: 'Second step', description: 'Step description'},
+    {mainTitle: 'Third step', description: 'Step description'},
+    {mainTitle: 'Forth step', description: 'Step description'},
+  ];
+
+  dangerWizardIndex = 1;
+  dangerWizardSteps = [
+    {mainTitle: 'First step', description: 'Step description'},
+    {mainTitle: 'Second step', description: 'Step description'},
+    {mainTitle: 'Third step', description: 'Step description'},
+    {mainTitle: 'Forth step', description: 'Step description'},
+  ];
+
+  successWizardIndex = 1;
+  successWizardSteps = [
+    {mainTitle: 'First step', description: 'Step description'},
+    {mainTitle: 'Second step', description: 'Step description'},
+    {mainTitle: 'Third step', description: 'Step description'},
+    {mainTitle: 'Forth step', description: 'Step description'},
+  ];
+
+  primaryArrowWizardIndex = 1;
+  primaryArrowWizardSteps = [
+    {mainTitle: 'First step', description: 'Step description'},
+    {mainTitle: 'Second step', description: 'Step description'},
+    {mainTitle: 'Third step', description: 'Step description'},
+    {mainTitle: 'Forth step', description: 'Step description'},
+  ];
+
+  dangerArrowWizardIndex = 1;
+  dangerArrowWizardSteps = [
+    {mainTitle: 'First step', description: 'Step description'},
+    {mainTitle: 'Second step', description: 'Step description'},
+    {mainTitle: 'Third step', description: 'Step description'},
+    {mainTitle: 'Forth step', description: 'Step description'},
+  ];
+
+  successArrowWizardIndex = 1;
+  successArrowWizardSteps = [
     {mainTitle: 'First step', description: 'Step description'},
     {mainTitle: 'Second step', description: 'Step description'},
     {mainTitle: 'Third step', description: 'Step description'},
@@ -21,6 +61,7 @@ export class FormsWizardComponent {
     {mainTitle: 'Second step', description: 'Step description'},
     {mainTitle: 'Third step', description: 'Step description'},
   ];
+
 
   firstStepForm = this.fb.group({
     username: [null, [Validators.required]],
@@ -38,6 +79,7 @@ export class FormsWizardComponent {
   thirdStepForm = this.fb.group({
     agreed: [false, [Validators.requiredTrue]],
   })
+
 
   onBeforeNext = () => {
     const forms = [this.firstStepForm, this.secondStepForm];
