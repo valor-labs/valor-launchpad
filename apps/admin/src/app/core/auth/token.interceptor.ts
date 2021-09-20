29c79e689a44b5bbf79634f88b334b59eb75717f
xiehongyang <>
2021-09-21 01:21:57 +0800

    fix(auth): add when api status 401 navigate to sign-in page, fixes #376

--- apps/admin/src/app/core/auth/token.interceptor.ts
+++ apps/admin/src/app/core/auth/token.interceptor.ts
@@ -3,14 +3,18 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor, HttpErrorResponse
 } from '@angular/common/http';
 import { AuthService } from './auth.service';
-import {Observable} from "rxjs";
+import { Observable, of, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-  constructor(public auth: AuthService) {}
+  constructor(public auth: AuthService, private router: Router) {
+  }
+
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
     request = request.clone({
@@ -18,6 +22,16 @@ export class TokenInterceptor implements HttpInterceptor {
         Authorization: `Bearer ${this.auth.getToken()}`
       }
     });
-    return next.handle(request);
+    return next.handle(request)
+      .pipe(
+        catchError(err => this._handleAuthError(err))
+      );
+  }
+
+  private _handleAuthError(err: HttpErrorResponse): Observable<any> {
+    if (err.status === 401 || err.status === 403) {
+      this.router.navigate(['/sign-in']);
+    }
+    return throwError(err);
   }
 }
--- apps/admin/src/app/core/header/header.component.ts
+++ apps/admin/src/app/core/header/header.component.ts
@@ -33,7 +33,7 @@ export class HeaderComponent implements OnInit {
         }))
       )
     );
- 
+
   profileActions: Action[] = [
     {
       label: 'Profile',
