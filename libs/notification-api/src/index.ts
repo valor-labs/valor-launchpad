23a140372b287fa0852b7ff757b2e581fc2da89b
ErikMac <erikyuyz@gmail.com>
2021-11-03 21:38:13 +0800

    refactor(notification-api): code clean and refactor

--- libs/notification-api/src/index.ts
+++ libs/notification-api/src/index.ts
@@ -1,3 +1,2 @@
 export * from './lib/notification-api.module';
-export * from './lib/socket-conn.service';
-export * from './lib/auth-socket-io.adapter';
+export * from './lib/notification.service';
--- libs/notification-api/src/lib/notification.service.ts
+++ libs/notification-api/src/lib/notification.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@valor-launchpad/prisma';
+import { SocketConnService } from '@valor-launchpad/socket-gateway';
+import { NotificationVo } from '@valor-launchpad/api-interfaces';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class NotificationService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private socketConnService: SocketConnService
+  ) {}
 
   async getNotifications(userId: string) {
     return await this.prisma.notification.findMany({
@@ -20,5 +26,72 @@ export class NotificationService {
         readDate: new Date(),
       },
     });
+  }
+
+  async createCommentNotification(projectId: string, actingUser) {
+    const project = await this.prisma.projectsEntity.findFirst({
+      include: {
+        summary: { select: { reporter: { select: { id: true } } } },
+        assignee: { select: { userId: true } },
+      },
+      where: { id: projectId },
+    });
+    const userIds = Array.from(
+      new Set([
+        project.summary.reporter.id,
+        ...project.assignee.map((i) => i.userId),
+      ])
+    );
+    for (const userId of userIds) {
+      const notification = await this.prisma.notification.create({
+        data: {
+          userId,
+          type: 'COMMENT',
+          read: false,
+          readDate: null,
+          extras: { project, actingUser } as unknown as Prisma.JsonObject,
+        },
+      });
+      this.notify(userId, notification);
+    }
+  }
+
+  async createReplyNotification(commentDto, actingUser) {
+    const comment = await this.prisma.commentEntity.findFirst({
+      where: { id: commentDto.commentId },
+    });
+    const notification = await this.prisma.notification.create({
+      data: {
+        userId: comment.author_id,
+        type: 'REPLY_COMMENT',
+        read: false,
+        readDate: null,
+        extras: { comment, actingUser } as unknown as Prisma.JsonObject,
+      },
+    });
+    this.notify(comment.author_id, notification);
+  }
+
+  async createLikeCommentNotification(commentId: string, actingUser) {
+    const comment = await this.prisma.commentEntity.findFirst({
+      where: { id: commentId },
+    });
+    const notification = await this.prisma.notification.create({
+      data: {
+        userId: comment.author_id,
+        type: 'LIKE_COMMENT',
+        read: false,
+        extras: { comment, actingUser } as unknown as Prisma.JsonObject,
+      },
+    });
+    this.notify(comment.author_id, notification);
+  }
+
+  private notify(userId: string, notification: NotificationVo) {
+    this.socketConnService.notifyByUserId(
+      userId,
+      'newNotification',
+      notification
+    );
   }
 }
