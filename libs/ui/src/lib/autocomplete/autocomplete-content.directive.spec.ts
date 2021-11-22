92ec0159c395fdcc4b5445aa38b0b1c58bf62339
sawyerbutton <linghui92liu@gmail.com>
2021-11-22 17:13:32 +0800

    trying add constructor in derective ut

--- libs/ui/src/lib/autocomplete/autocomplete-content.directive.spec.ts
+++ libs/ui/src/lib/autocomplete/autocomplete-content.directive.spec.ts
@@ -1,8 +1,21 @@
 import { AutocompleteContentDirective } from './autocomplete-content.directive';
+import { TemplateRef } from '@angular/core';
 
 describe('AutocompleteContentDirective', () => {
+  let templateRef: createSpyObj<TemplateRef<any>>;
   it('should create an instance', () => {
     const directive = new AutocompleteContentDirective();
     expect(directive).toBeTruthy();
   });
 });
+
+
+export const createSpyObj = (baseName, methodNames): { [key: string]: jest.Mock<any> } => {
+  const obj: any = {};
+
+  for (let i = 0; i < methodNames.length; i++) {
+    obj[methodNames[i]] = jest.fn();
+  }
+
+  return obj;
+};
