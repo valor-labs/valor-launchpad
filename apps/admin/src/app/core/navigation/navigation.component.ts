5b1d5f53370e9b315fbb6a7a43da8d9a387c3575
steve <xhysteve@gmail.com>
2021-12-10 15:11:05 +0800

    fix lint error

--- apps/admin/src/app/core/navigation/navigation.component.ts
+++ apps/admin/src/app/core/navigation/navigation.component.ts
@@ -15,6 +15,11 @@ import { SidebarItemComponent } from './sidebar-item/sidebar-item.component';
   styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent implements AfterViewInit {
+
+  @ViewChildren(SidebarItemComponent)
+  private sidebarItems: QueryList<SidebarItemComponent>;
+
+  @Input() menus = [];
   subMenuCollapseState = {
     projects: true,
   };
@@ -25,11 +30,6 @@ export class NavigationComponent implements AfterViewInit {
       <HTMLElement>document.getElementsByClassName('js-simplebar')[0]
     );
   }
-
-  @ViewChildren(SidebarItemComponent)
-  private sidebarItems: QueryList<SidebarItemComponent>;
-
-  @Input() menus = [];
 
   constructor(public navigationService: NavigationService) {
     //  TODO: Make the menu state reflect the navigation tree
