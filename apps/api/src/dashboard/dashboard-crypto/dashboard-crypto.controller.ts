3298dda8829cbb727481973001a450ed1875e46b
sheldon <yjayjamb@163.com>
2021-12-15 22:36:28 +0800

    fix(tasks): output error log

--- apps/api/src/dashboard/dashboard-crypto/dashboard-crypto.controller.ts
+++ apps/api/src/dashboard/dashboard-crypto/dashboard-crypto.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '@valor-launchpad/auth-api';
 import { DashboardCryptoService } from './dashboard-crypto.service';
+import { ResponseError, ResponseSuccess } from '@valor-launchpad/common-api';
 
 @Controller('v1')
 @UseGuards(JwtAuthGuard)
@@ -8,22 +9,45 @@ export class DashboardCryptoController {
   constructor(private dashboardCryptoService: DashboardCryptoService) {}
 
   @Get('crypto-main-info')
-  getCryptoMainInfo() {
-    return this.dashboardCryptoService.getCryptoMainInfo();
+  async getCryptoMainInfo() {
+    try {
+      const mainInfo = await this.dashboardCryptoService.getCryptoMainInfo();
+
+      return new ResponseSuccess('Load Main Info Success', mainInfo);
+    } catch (err) {
+      return new ResponseError('Load Main Info Failed', err);
+    }
   }
 
   @Get('crypto-markets')
-  getCryptoMarkets() {
-    return this.dashboardCryptoService.getCryptoMarkets();
+  async getCryptoMarkets() {
+    try {
+      const markets = await this.dashboardCryptoService.getCryptoMarkets();
+
+      return new ResponseSuccess('Load Markets Success', markets);
+    } catch (err) {
+      return new ResponseError('Load Markets Failed', err);
+    }
   }
 
   @Get('crypto-orders')
-  getCryptoOrders() {
-    return this.dashboardCryptoService.getCryptoOrders();
+  async getCryptoOrders() {
+    try {
+      const orders = await this.dashboardCryptoService.getCryptoOrders();
+
+      return new ResponseSuccess('Load Orders Success', orders);
+    } catch (err) {
+      return new ResponseError('Load Markets Failed', err);
+    }
   }
 
   @Get('crypto-k-lines')
-  getCryptoKLines() {
-    return this.dashboardCryptoService.getCryptoKLines();
+  async getCryptoKLines() {
+    try {
+      const klines = await this.dashboardCryptoService.getCryptoKLines();
+      return new ResponseSuccess('Load KLines Success', klines);
+    } catch (err) {
+      return new ResponseError('Load KLines Failed', err);
+    }
   }
 }
