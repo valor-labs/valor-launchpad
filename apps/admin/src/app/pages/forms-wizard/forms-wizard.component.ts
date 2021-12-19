8ea791af66f50b3292da61d4d3382bd8324c7fa9
weishax <2632350349@qq.com>
2021-12-19 20:16:30 +0800

    fix: format

--- apps/admin/src/app/pages/forms-wizard/forms-wizard.component.ts
+++ apps/admin/src/app/pages/forms-wizard/forms-wizard.component.ts
@@ -65,7 +65,13 @@ export class FormsWizardComponent {
   firstStepForm = this.fb.group({
     username: [null, [Validators.required]],
     password: [null, [Validators.required]],
-    confirmedPassword: [null, [Validators.required, this.validatePassWordEqual('password', 'confirmedPassword')]],
+    confirmedPassword: [
+      null,
+      [
+        Validators.required,
+        this.validatePassWordEqual('password', 'confirmedPassword'),
+      ],
+    ],
   });
 
   secondStepForm = this.fb.group({
@@ -87,7 +93,7 @@ export class FormsWizardComponent {
     agreed: [false, [Validators.requiredTrue]],
   });
 
-  confirmedTip = 'This field is required.'
+  confirmedTip = 'This field is required.';
 
   onBeforeNext = () => {
     const forms = [this.firstStepForm, this.secondStepForm];
@@ -103,29 +109,34 @@ export class FormsWizardComponent {
   };
   constructor(private fb: FormBuilder) {}
 
-  validatePassWordEqual(passwordKey: string, confirmedPasswordKey: string): ValidatorFn {
-    return (): {[key: string]: any} => {
+  validatePassWordEqual(
+    passwordKey: string,
+    confirmedPasswordKey: string
+  ): ValidatorFn {
+    return (): { [key: string]: any } => {
       if (this.firstStepForm) {
         const password = this.firstStepForm.controls[passwordKey];
-        const confirmedPassword = this.firstStepForm.controls[confirmedPasswordKey];
+        const confirmedPassword =
+          this.firstStepForm.controls[confirmedPasswordKey];
 
         password.valueChanges.subscribe(() => {
-          confirmedPassword.updateValueAndValidity()
-        })
+          confirmedPassword.updateValueAndValidity();
+        });
 
-        confirmedPassword.valueChanges.subscribe(value => {
+        confirmedPassword.valueChanges.subscribe((value) => {
           if (value) {
-            this.confirmedTip = 'Passwords are not equal.'
+            this.confirmedTip = 'Passwords are not equal.';
           } else {
-            this.confirmedTip = 'This field is required.'
+            this.confirmedTip = 'This field is required.';
           }
-        })
-  
-        return password.value === confirmedPassword.value ? null : { validatePassWordEqual: true }
+        });
+
+        return password.value === confirmedPassword.value
+          ? null
+          : { validatePassWordEqual: true };
       }
 
-      return null
-    }
+      return null;
+    };
   }
-
 }
